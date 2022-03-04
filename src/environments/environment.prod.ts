b7b530119a8c1d58e325ccbd03ce38d9a4cd328b
Alexandrress <alexandress12355@hotmail.com>
2022-03-04 23:23:50 +0100

    feat: generated deployables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  api: "https://app-ec21e68e-3e55-42d7-b1ae-3eef7507a353.cleverapps.io/",
+  mail: "alexandre.leveque@ig2i.centralelille.fr",
+  mdp: "happyfriday59"
 };
