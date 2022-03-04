2e1855b1c55cc6dac23ad9b8da09f4d7b693d9ad
Alexandrress <alexandress12355@hotmail.com>
2022-03-04 23:44:40 +0100

    fix: changed credentials to have the standard account by default

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,8 @@
 export const environment = {
   production: false,
   api: "https://app-ec21e68e-3e55-42d7-b1ae-3eef7507a353.cleverapps.io/",
-  mail: "alexandre.leveque@ig2i.centralelille.fr",
-  mdp: "happyfriday59"
+  mail: "francois.pouls@ig2i.centralelille.fr",
+  mdp: "toto12345"
 };
 
 /*
